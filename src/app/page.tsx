5c4bc4b0a1cb2634fedd157bb8667ad082dd56d6
Kerri Morris <kkmorrisfam@hotmail.com>
2025-05-20 16:17:25 -0700

    connect to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
 import Image from "next/image";
+    // File: app/page.tsx
+import { neon } from '@neondatabase/serverless';
 
-export default function Home() {
+export default function Page() {
+  async function create(formData: FormData) {
+    'use server';
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+    // Insert the comment from the form into the Postgres database
+    await sql`INSERT INTO comments (comment) VALUES (${comment})`;
+  }
+
+  return (
+    <form action={create}>
+      <input type="text" placeholder="write a comment" name="comment" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -99,5 +119,6 @@ export default function Home() {
         </a>
       </footer>
     </div>
+
   );
 }
